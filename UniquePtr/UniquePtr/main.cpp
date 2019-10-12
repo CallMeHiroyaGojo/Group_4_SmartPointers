89c7d73b4fc9fb1da908d414424592266d11e8b3
hlainggithub <sulanmi2@gmail.com>
2019-10-11 22:22:04 -0700

    I would like to add this file,please add it

---
+++ UniquePtr/UniquePtr/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UniquePtr
+//
+//  Created by Ojousama on 10/11/19.
+//  Copyright © 2019 Ojousama. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
